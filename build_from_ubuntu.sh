d4f46de15d7e92ae7b5ff148b9711cefe25b7119
xtangxtang <xi.tang@intel.com>
2024-09-03 15:48:45 +0800

    update

--- build_from_ubuntu.sh
+++ build_from_ubuntu.sh
@@ -1,58 +1,80 @@
 #!/bin/bash -e
 
-# Update packages
+FAISS_REPO=https://github.com/intel-sandbox/xps-prc-ds-faiss-opt.git
+
+# 更新系统软件包
 apt-get update -y
-apt install -y build-essential wget git vim software-properties-common lsb-release
+apt-get upgrade -y
 
-# Change working directory
-#cd /home/
+# 安装编译工具
+apt-get install -y build-essential
 
-# Install Latest CMake
+# 安装 wget、git 和 vim
+apt-get install -y wget git vim
+
+# 安装软件包管理工具
+apt-get install -y software-properties-common
+
+# 安装 lsb-release
+apt-get install -y lsb-release
+
+# 添加 Kitware 仓库和 Key
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
-apt update -y
-apt install kitware-archive-keyring -y
-apt update -y
-apt install cmake -y
-apt install -y build-essential libtool autoconf unzip wget
+apt-add-repository 'deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main'
+apt-get update -y
 
-# Install Miniconda
-rm -rf /root/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
-chmod a+x Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
-bash ./Miniconda3-py310_23.5.2-0-Linux-x86_64.sh -b
+# 安装 CMake
+apt-get install -y cmake
 
-# Install tools and libraries
-apt-get install -y linux-tools-common linux-tools-generic swig python3-dev python3-numpy-dev python3-pip
-apt install -y linux-tools-6.2.0-36-generic
+# 安装构建工具和依赖库
+apt-get install -y libtool autoconf unzip wget
 
-# Download and prepare faiss
-#wget https://github.com/facebookresearch/faiss/archive/refs/tags/v1.7.4.tar.gz
-#tar -xvf v1.7.4.tar.gz
-wget https://github.com/facebookresearch/faiss/archive/refs/tags/v1.8.0.tar.gz
-tar -xvf v1.8.0.tar.gz
-mv faiss-1.8.0 ../faiss
+# 下载 Miniconda
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+chmod a+x Miniforge3-Linux-x86_64.sh
+bash ./Miniforge3-Linux-x86_64.sh -b -u
 
-# Assuming you have 'run_conda.sh', 'run_conda_env.sh', and 'build_faiss.sh' in your current directory
-cp run_conda.sh ../faiss/
-cp run_conda_env.sh ../faiss/
-chmod a+x ../faiss/run_conda.sh ../faiss/run_conda_env.sh
+echo 'export PATH=/root/miniforge3/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 
-# Setup build for faiss
-cp build_faiss.sh ../faiss/
-chmod a+x ../faiss/build_faiss.sh
+# 安装 Linux 性能分析工具
+apt-get install -y linux-tools-common linux-tools-generic linux-tools-$(uname -r)
+apt-get install -y nmon htop
 
-# Python package installations
+# 下载 Faiss 源代码
+# url=$FAISS_REPO
+# FAISS_FOLDER=$(echo $url | sed 's|.*/||; s|.git$||')
+# echo $FAISS_FOLDER
+
+# rm -rf $FAISS_FOLDER
+# git clone $FAISS_REPO
+
+FAISS_FOLDER=/home/xtang/faiss-1.8.0/
+
+# 添加自定义脚本并赋予执行权限
+cp run_conda.sh $FAISS_FOLDER/
+cp run_conda_env.sh $FAISS_FOLDER/
+cp build_faiss_miniforge.sh $FAISS_FOLDER/
+chmod a+x $FAISS_FOLDER/run_conda.sh
+chmod a+x $FAISS_FOLDER/run_conda_env.sh
+chmod a+x $FAISS_FOLDER/build_faiss_miniforge.sh
+
+# 安装 SWIG
+pip3 install swig==4.1.0
+# 安装 Python 开发依赖
+apt-get install -y python3-dev python3-pip
+pip3 install numpy
+
+# 安装 setuptools
 pip3 install setuptools
 
-# Clone necessary repositories
-#git clone https://github.com/epeshared/faiss_perf.git
-cd ../
+# 克隆 oneDNN 库
+rm -rf oneDNN
 git clone https://github.com/oneapi-src/oneDNN.git
-
-rm -rf v1.8.0.tar.gz
-rm -rf Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
-
-# Note: To run the scripts inside faiss directory, navigate to /home/faiss and execute them as needed.
-
-
+mv oneDNN ../oneDNN
+cd ../oneDNN
+mkdir -p build
+cd build
+cmake ..
+make -j
+make install
--- run_conda_env_miniforge.sh
+++ run_conda_env_miniforge.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
 # conda install -c pytorch faiss-cpu=1.7.4 mkl=2023 blas=1.0=mkl
+source ~/.bashrc
+conda config --remove channels defaults
+conda config --remove channels conda-forge
 conda install -c pytorch mkl=2023 blas=1.0=mkl
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/root/miniforge3/lib/:/usr/local/lib64/"
 ln -s /root/miniforge3/lib/libmkl_intel_lp64.so.2 /root/miniforge3/lib/libmkl_intel_lp64.so.1
